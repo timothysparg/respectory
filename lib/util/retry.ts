5c79dc397d71206cd79e1a572ed6b6021269b649
Tim Sparg <timothy.sparg@gmail.com>
2020-03-05 10:51:34 +0100

    Closes #24

--- lib/util/retry.ts
+++ lib/util/retry.ts
@@ -1,8 +1,7 @@
 import promiseRetry = require("promise-retry");
 import { WrapOptions } from "retry";
 
-// TODO #24
-// import { logger } from "./logger";
+import { logger } from "./logger";
 
 /**
  * Default retry options for doWithRetry.
@@ -36,15 +35,13 @@ export function doWithRetry<R>(what: () => Promise<R>,
         ...opts,
     };
     if (opts.log) {
-        // TODO #24
-        // logger.log("silly", `${description} with retry options '%j'`, retryOptions);
+        logger.log("silly", `${description} with retry options '%j'`, retryOptions);
     }
     return promiseRetry(retryOptions, retry => {
         return what()
             .catch(err => {
                 if (opts.log) {
-                    // TODO #24
-                    // logger.warn(`Error occurred attempting '${description}': ${err.message}`);
+                    logger.warn(`Error occurred attempting '${description}': ${err.message}`);
                 }
                 retry(err);
             });
